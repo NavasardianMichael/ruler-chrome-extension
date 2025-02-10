67f264aa51fa563fbdecf7dcf8a10b8fe0bdf4b8
NavasardianMichael <navasardianmichael@gmail.com>
2025-02-10 20:59:16 +0400

    started dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite';
-import { crx } from '@crxjs/vite-plugin';
-import manifest from './src/manifest';
+import { defineConfig } from "vite";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./src/manifest";
 
 export default defineConfig({
-    plugins: [
-        crx({ manifest }),
-    ],
-    server: {
-        port: 5575,
-        open: true
-    }
+  plugins: [crx({ manifest })],
+  build: {
+    minify: "esbuild",
+  },
+  server: {
+    port: 5575,
+    open: true,
+  },
 });

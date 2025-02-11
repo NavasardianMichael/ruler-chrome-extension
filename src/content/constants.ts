c5e523b040d661f323fa26c51e581af4595b1e7a
NavasardianMichael <navasardianmichael@gmail.com>
2025-02-11 19:56:46 +0400

    bzbzum enq

---
+++ src/content/constants.ts
@@ -0,0 +1,5 @@
+export const DIMENSIONS = {
+  lineAndSizeGap: 4,
+  measuredSize: 12,
+  lineWidth: 1,
+};
--- src/content/main.ts
+++ src/content/main.ts
@@ -1,183 +1,224 @@
-import styles from './styles.module.css'
+import { DIMENSIONS } from "./constants";
+import styles from "./styles.module.css";
 
 type State = {
-  isCtrlDown: boolean
-  anchorElement: HTMLElement | null
-  targetElement: HTMLElement | null
-}
+  isCtrlDown: boolean;
+  anchorElement: HTMLElement | null;
+  targetElement: HTMLElement | null;
+};
 
 const state: State = {
   isCtrlDown: false,
   anchorElement: null,
   targetElement: null,
-}
+};
 
 const highlightElement = (element: HTMLElement) => {
-  if (!element) return
-  element.classList.add(styles.highlighted)
-}
+  if (!element) return;
+  element.classList.add(styles.highlighted);
+};
 
 const unHighlightElement = (element: HTMLElement) => {
-  if (!element) return
-  element.classList.remove(styles.highlighted)
-}
+  if (!element) return;
+  element.classList.remove(styles.highlighted);
+};
 const pinElement = (element: HTMLElement) => {
-  if (!element) return
-  element.classList.add(styles.pinned)
-}
+  if (!element) return;
+  element.classList.add(styles.pinned);
+};
 
 const unPinElement = (element: HTMLElement) => {
-  if (!element) return
-  element.classList.remove(styles.pinned)
-}
+  if (!element) return;
+  element.classList.remove(styles.pinned);
+};
 
-const removeLines = () => {
-  Array.from(document.querySelectorAll(styles.line)).forEach((lineElement) => {
-    lineElement.remove()
-  })
-}
+const removePaintings = () => {
+  Array.from(document.querySelectorAll(`.${styles.line}`)).forEach(
+    (lineElement) => {
+      lineElement.remove();
+    }
+  );
+  Array.from(document.querySelectorAll(`.${styles.size}`)).forEach(
+    (lineElement) => {
+      lineElement.remove();
+    }
+  );
+};
 
-const paintLines = (x1: number, y1: number, x2: number, y2: number) => {
-  const fragment = document.createDocumentFragment()
+const paintLines = (x1: number, x2: number, y1: number, y2: number) => {
+  const fragment = document.createDocumentFragment();
+  console.log({ x1, x2 });
 
   if (x1 !== x2) {
-    const horizontalLine = document.createElement('div')
-    horizontalLine.classList.add(styles.line)
-    horizontalLine.style.top = `${y1}px`
-    horizontalLine.style.left = `${x1}px`
-    horizontalLine.style.width = `${x2 - x1}px`
-    fragment.appendChild(horizontalLine)
-    console.log({ horizontalLine })
+    const horizontalLineWidth = Math.abs(Math.round(x2 - x1));
+    console.log({ horizontalLineWidth });
+
+    const horizontalLine = document.createElement("div");
+    horizontalLine.classList.add(styles.line);
+    horizontalLine.style.top = `${y1}px`;
+    horizontalLine.style.left = `${x1}px`;
+    horizontalLine.style.width = `${horizontalLineWidth}px`;
+    fragment.appendChild(horizontalLine);
+
+    const horizontalLineWidthSizeBlock = document.createElement("p");
+    horizontalLineWidthSizeBlock.classList.add(styles.size);
+    horizontalLineWidthSizeBlock.style.top = `${
+      y1 - DIMENSIONS.lineAndSizeGap - DIMENSIONS.lineWidth
+    }px`;
+    horizontalLineWidthSizeBlock.style.left = `${
+      x1 + horizontalLineWidth / 2
+    }px`;
+    horizontalLineWidthSizeBlock.innerHTML = `${horizontalLineWidth}px`;
+    fragment.appendChild(horizontalLineWidthSizeBlock);
   }
 
+  console.log({ y1, y2 });
   if (y1 !== y2) {
-    const verticalLine = document.createElement('div')
-    verticalLine.classList.add(styles.line)
-    verticalLine.style.left = `${x1}px`
-    verticalLine.style.top = `${y1}px`
-    verticalLine.style.height = `${y2 - y1}px`
-    fragment.appendChild(verticalLine)
-    console.log({ verticalLine })
+    const verticalLineHeight = Math.abs(Math.round(y2 - y1));
+    console.log({ verticalLineHeight });
+
+    const verticalLine = document.createElement("div");
+    verticalLine.classList.add(styles.line);
+    verticalLine.style.left = `${x1}px`;
+    verticalLine.style.top = `${y1}px`;
+    verticalLine.style.height = `${verticalLineHeight}px`;
+    fragment.appendChild(verticalLine);
+
+    const verticalLineHeightSizeBlock = document.createElement("span");
+    verticalLineHeightSizeBlock.classList.add(styles.size);
+    verticalLineHeightSizeBlock.style.left = `${
+      x1 + DIMENSIONS.lineAndSizeGap - DIMENSIONS.lineWidth
+    }px`;
+    verticalLineHeightSizeBlock.style.top = `${y1 + verticalLineHeight / 2}px`;
+    verticalLineHeightSizeBlock.innerHTML = `${verticalLineHeight}px`;
+    fragment.appendChild(verticalLineHeightSizeBlock);
   }
 
-  if (!fragment.children.length) return
+  if (!fragment.children.length) return;
 
-  document.body.appendChild(fragment)
-}
+  document.body.appendChild(fragment);
+};
 
 const initRuler = (anchorElement: HTMLElement, targetElement: HTMLElement) => {
   const {
-    left: anchorLeft,
-    top: anchorTop,
+    left: anchorX,
+    top: anchorY,
     height: anchorHeight,
     width: anchorWidth,
-  } = anchorElement.getBoundingClientRect()
+  } = anchorElement.getBoundingClientRect();
   const {
-    left: targetLeft,
-    top: targetTop,
+    left: targetX,
+    top: targetY,
     height: targetHeight,
     width: targetWidth,
-  } = targetElement.getBoundingClientRect()
+  } = targetElement.getBoundingClientRect();
 
-  const isTargetFromRight = anchorLeft < targetLeft
-  const sameHorizontalLine = anchorLeft === targetLeft
-  const isTargetFromTop = anchorTop > targetTop
-  const sameVerticalLine = anchorTop === targetTop
-  console.log({ targetElement, anchorElement })
+  const hasHorizontalSpace = anchorX + anchorWidth !== targetX + targetWidth;
+  const hasVerticalSpace = anchorY + anchorHeight !== targetY + targetHeight;
 
-  const x1 =
-    sameHorizontalLine || isTargetFromRight
-      ? anchorLeft + anchorWidth
-      : anchorLeft
-  const x2 =
-    sameHorizontalLine || isTargetFromRight
-      ? targetLeft
-      : targetLeft + targetWidth
-  const y1 = sameVerticalLine
-    ? anchorTop
-    : isTargetFromTop
-    ? targetTop + targetHeight
-    : anchorTop + anchorHeight
-  const y2 = sameVerticalLine || isTargetFromTop ? anchorTop : targetTop
-  console.log({ x1, x2, y1, y2 })
+  const isTargetFromRight = anchorX < targetX;
+  const leftElement = isTargetFromRight ? anchorElement : targetElement;
+  const { left: leftElementX, width: leftElementWidth } =
+    leftElement.getBoundingClientRect();
 
-  paintLines(x1, x2, y1, y2)
-}
+  const rightElement = isTargetFromRight ? targetElement : anchorElement;
+  const { left: rightElementX } = rightElement.getBoundingClientRect();
+
+  const isTargetFromTop = anchorY > targetY;
+  const topElement = isTargetFromTop ? targetElement : anchorElement;
+  const { top: topElementY, height: topElementWidth } =
+    topElement.getBoundingClientRect();
+
+  const bottomElement = isTargetFromTop ? anchorElement : targetElement;
+  const { top: bottomElementY } = bottomElement.getBoundingClientRect();
+
+  const x1 = leftElementX + leftElementWidth;
+  const x2 = rightElementX;
+  const y1 = topElementY + topElementWidth;
+  const y2 = bottomElementY;
+
+  paintLines(
+    hasHorizontalSpace ? x1 : 0,
+    hasHorizontalSpace ? x2 : 0,
+    hasVerticalSpace ? y1 : 0,
+    hasVerticalSpace ? y2 : 0
+  );
+};
 
 const mouseOverTargetElementHandler = (event: MouseEvent) => {
-  unHighlightElement(state.targetElement!)
-  state.targetElement = null
-  removeLines()
+  unHighlightElement(state.targetElement!);
+  state.targetElement = null;
+  removePaintings();
 
-  const element = event.target as HTMLElement
-  state.targetElement = element
-  highlightElement(state.targetElement)
+  const element = event.target as HTMLElement;
+  state.targetElement = element;
+  highlightElement(state.targetElement);
 
-  initRuler(state.anchorElement!, state.targetElement)
-}
+  initRuler(state.anchorElement!, state.targetElement);
+};
 
 const clickTargetElementHandler = (event: MouseEvent) => {
-  const element = event.target as HTMLElement
-  unHighlightElement(state.anchorElement!)
-  unPinElement(state.anchorElement!)
+  const element = event.target as HTMLElement;
+  unHighlightElement(state.anchorElement!);
+  unPinElement(state.anchorElement!);
 
-  pinElement(element)
-  state.anchorElement = element
+  pinElement(element);
+  state.anchorElement = element;
 
-  unHighlightElement(state.targetElement!)
-  state.targetElement = null
-  removeLines()
+  unHighlightElement(state.targetElement!);
+  state.targetElement = null;
+  removePaintings();
 
-  document.removeEventListener('mouseover', mouseOverAnchorElementHandler)
-  document.addEventListener('mouseover', mouseOverTargetElementHandler)
-}
+  document.removeEventListener("mouseover", mouseOverAnchorElementHandler);
+  document.addEventListener("mouseover", mouseOverTargetElementHandler);
+};
 
 const mouseOverAnchorElementHandler = (event: MouseEvent) => {
-  unHighlightElement(state.anchorElement as HTMLElement)
+  unHighlightElement(state.anchorElement as HTMLElement);
 
-  unHighlightElement(state.targetElement!)
-  state.targetElement = null
-  removeLines()
+  unHighlightElement(state.targetElement!);
+  state.targetElement = null;
+  removePaintings();
 
-  const element = event.target as HTMLElement
-  state.anchorElement = element
-  highlightElement(state.anchorElement)
+  const element = event.target as HTMLElement;
+  state.anchorElement = element;
+  highlightElement(state.anchorElement);
 
-  document.addEventListener('click', clickTargetElementHandler)
-}
+  document.addEventListener("click", clickTargetElementHandler);
+};
 
 const keyDownHandler = (event: KeyboardEvent) => {
-  if (event.key !== 'Control' || state.isCtrlDown) return
-  state.isCtrlDown = true
-  document.addEventListener('mouseover', mouseOverAnchorElementHandler)
-}
+  if (event.key !== "Control" || state.isCtrlDown) return;
+  state.isCtrlDown = true;
+  document.addEventListener("mouseover", mouseOverAnchorElementHandler);
+};
 
 const keyUpHandler = (event: KeyboardEvent) => {
-  if (event.ctrlKey) return
-  state.isCtrlDown = false
-  document.removeEventListener('mouseover', mouseOverAnchorElementHandler)
-  document.removeEventListener('mouseover', mouseOverTargetElementHandler)
-  document.removeEventListener('click', clickTargetElementHandler)
+  if (event.ctrlKey) return;
+  state.isCtrlDown = false;
+  document.removeEventListener("mouseover", mouseOverAnchorElementHandler);
+  document.removeEventListener("mouseover", mouseOverTargetElementHandler);
+  document.removeEventListener("click", clickTargetElementHandler);
 
-  unHighlightElement(state.anchorElement!)
-  unPinElement(state.anchorElement!)
-  state.anchorElement = null
+  unHighlightElement(state.anchorElement!);
+  unPinElement(state.anchorElement!);
+  state.anchorElement = null;
 
-  unHighlightElement(state.targetElement!)
-  state.targetElement = null
-  removeLines()
-}
+  unHighlightElement(state.targetElement!);
+  state.targetElement = null;
+  removePaintings();
+};
 
 const init = () => {
-  document.addEventListener('keydown', keyDownHandler)
-  document.addEventListener('keyup', keyUpHandler)
-}
+  document.addEventListener("keydown", keyDownHandler);
+  document.addEventListener("keyup", keyUpHandler);
+};
 
 if (
-  document.readyState === 'complete' ||
-  document.readyState === 'interactive'
+  document.readyState === "complete" ||
+  document.readyState === "interactive"
 ) {
-  init()
+  init();
 } else {
-  document.addEventListener('DOMContentLoaded', init)
+  document.addEventListener("DOMContentLoaded", init);
 }
